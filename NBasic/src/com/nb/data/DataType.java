129253ba239f2e605a766647af72bb214eed1039
Jervac <jervac>
2015-10-09 17:38:05 -0400

    Added Datatype enum for things such as Strings and Ints

---
+++ NBasic/src/com/nb/data/DataType.java
@@ -0,0 +1,5 @@
+package com.nb.data;
+
+public enum DataType {
+	INT, STRING;
+}
--- NBasic/src/com/nb/data/Variable.java
+++ NBasic/src/com/nb/data/Variable.java
@@ -1,5 +1,14 @@
 package com.nb.data;
 
 public class Variable {
-
+	
+	
+	
+	DataType dataType;
+	
+	
+	
+	public Variable(DataType dataType) {
+		this.dataType = dataType;
+	}
 }
