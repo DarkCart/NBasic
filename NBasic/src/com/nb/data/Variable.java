9a3da2f853d884f2528f41dd20a4190e219316d7
John Whiteaker <darkcartgames@gmail.com>
2015-10-09 19:06:50 -0500

    Update Variable.java

--- NBasic/src/com/nb/data/Variable.java
+++ NBasic/src/com/nb/data/Variable.java
@@ -1,14 +1,25 @@
 package com.nb.data;
 
 public class Variable {
+	DataType dataType = DataType.NULL;
+	String name = "";
+	String value = "";
 	
-	
-	
-	DataType dataType;
-	
-	
-	
-	public Variable(DataType dataType) {
+	public Variable(String name, String value, DataType dataType) {
 		this.dataType = dataType;
+		this.name = name;
+		this.value = value;
+	}
+	
+	public DataType getDataType() {
+		return dataType;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getValue() {
+		return value;
 	}
 }
